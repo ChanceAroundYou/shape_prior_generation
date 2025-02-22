c428f73627f98d628c7a664a3e816696856d7b58
Chance <chenranlin.17@gmail.com>
2025-02-22 17:34:11 +0800

    ``` 重构代码以优化数据加载和处理逻辑 ```

--- models/ResVAE.py
+++ models/ResVAE.py
@@ -1,5 +1,4 @@
 import torch
-import torch.nn.functional as F
 from torch import nn
 
 
@@ -10,23 +9,26 @@ class ResLinearBlock(nn.Module):
             nn.Linear(in_features, hidden_features, bias=bias),
             nn.ReLU(),
             # nn.BatchNorm1d(hidden_features)
-            nn.LayerNorm(hidden_features)
+            nn.LayerNorm(hidden_features),
         )
-        self.hidden_layers = nn.Sequential(*[
-            nn.Sequential(
-                nn.Linear(hidden_features, hidden_features, bias=bias),
-                nn.ReLU(),
-                # nn.BatchNorm1d(hidden_features)
-                nn.LayerNorm(hidden_features)
+        self.hidden_layers = nn.Sequential(
+            *[
+                nn.Sequential(
+                    nn.Linear(hidden_features, hidden_features, bias=bias),
+                    nn.ReLU(),
+                    # nn.BatchNorm1d(hidden_features)
+                    nn.LayerNorm(hidden_features),
                 )
-                for _ in range(repeat)])
+                for _ in range(repeat)
+            ]
+        )
         self.output_layer = nn.Sequential(
             nn.Linear(hidden_features, out_features, bias=bias),
             nn.ReLU(),
             # nn.BatchNorm1d(out_features)
-            nn.LayerNorm(out_features)
+            nn.LayerNorm(out_features),
         )
-        
+
         self.in_features = in_features
         self.out_features = out_features
         self.hidden_features = hidden_features
@@ -36,11 +38,12 @@ class ResLinearBlock(nn.Module):
         x1 = self.hidden_layers(x1)
         x1 = self.output_layer(x1)
         if self.in_features != self.out_features:
-            x = x[:, :self.out_features]
+            x = x[:, : self.out_features]
         return x1 + x
-    
+
+
 class ResVariationalAutoEncoder(nn.Module):
-    def __init__(self, input_dim, h_dim=200, h_layers=[2,2], z_dim=2):
+    def __init__(self, input_dim, h_dim=200, h_layers=[2, 2], z_dim=2):
         super().__init__()
         # encoder
         self.img2hid = nn.Sequential(
@@ -49,10 +52,9 @@ class ResVariationalAutoEncoder(nn.Module):
             # nn.BatchNorm1d(h_dim)
             # nn.LayerNorm(h_dim)
         )
-        self.encoder_layers = nn.Sequential(*[
-            ResLinearBlock(h_dim, h_dim, h_dim, repeat=repeat)
-            for repeat in h_layers
-        ])
+        self.encoder_layers = nn.Sequential(
+            *[ResLinearBlock(h_dim, h_dim, h_dim, repeat=repeat) for repeat in h_layers]
+        )
         self.hid2mu = nn.Linear(h_dim, z_dim)
         self.hid2log_var = nn.Linear(h_dim, z_dim)
 
@@ -63,10 +65,9 @@ class ResVariationalAutoEncoder(nn.Module):
             # nn.BatchNorm1d(h_dim)
             # nn.LayerNorm(h_dim)
         )
-        self.decoder_layers = nn.Sequential(*[
-            ResLinearBlock(h_dim, h_dim, h_dim, repeat=repeat)
-            for repeat in h_layers
-        ])
+        self.decoder_layers = nn.Sequential(
+            *[ResLinearBlock(h_dim, h_dim, h_dim, repeat=repeat) for repeat in h_layers]
+        )
         self.hid2img = nn.Sequential(
             nn.Linear(h_dim, input_dim),
             # nn.ReLU(),
@@ -98,39 +99,45 @@ class ResVariationalAutoEncoder(nn.Module):
         z = self.sample(mu, log_var)
         x = self.decode(z)
         return x, mu, log_var
-    
+
     def sample(self, mu, log_var):
         sigma = log_var.exp().sqrt()
         epsilon = torch.randn_like(sigma)
         z = mu + sigma * epsilon
         return z
-    
+
     def generate(self, z):
         x = self.decode(z)
         return x
-    
+
     def reconstruct(self, h, eps=1e-8):
-        h = 1 / (torch.exp(h)+eps)
+        h = 1 / (torch.exp(h) + eps)
         h = h / torch.sum(h, dim=1, keepdim=True) * 2 * torch.pi
+        h = h.cumsum(dim=1)
         return h
-    
-    def loss(self, x, x_reconstructed, mu, log_var, kl_rate=0.01):
+
+    def loss(self, x, x_reconstructed, mu, log_var, kl_rate=0.1, true_rate=0.1):
         # Reconstruction loss
         # recon_loss = F.binary_cross_entropy(x_reconstructed, x, reduction='sum')
         recon_loss = (x_reconstructed - x).pow(2).sum(dim=1).mean()
-        true_loss = (self.reconstruct(x) - self.reconstruct(x_reconstructed)).pow(2).sum(dim=1).mean()
+        true_loss = (
+            (self.reconstruct(x) - self.reconstruct(x_reconstructed))
+            .pow(2)
+            .sum(dim=1)
+            .mean()
+        )
 
         # KL divergence loss
         kl_loss = mu.pow(2) + log_var.exp() - log_var - 1
         kl_loss = 0.5 * torch.sum(kl_loss, dim=1).mean()
-        loss = recon_loss + kl_rate * kl_loss
+        loss = recon_loss + true_rate * true_loss + kl_rate * kl_loss
         return {
-            'loss': loss,
-            'recon_loss': recon_loss,
-            'kl_loss': kl_loss,
-            'true_loss': true_loss
+            "loss": loss,
+            "recon_loss": recon_loss,
+            "kl_loss": kl_loss,
+            "true_loss": true_loss,
         }
-    
+
     def initial(self):
         for m in self.modules():
             if isinstance(m, nn.Linear):
@@ -139,9 +146,10 @@ class ResVariationalAutoEncoder(nn.Module):
                     nn.init.constant_(m.bias, 0)
                 except:
                     pass
-                
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     from torchsummary import summary
+
     model = ResVariationalAutoEncoder(100, h_dim=1000, h_layers=[1], z_dim=2)
-    summary(model, (100,), device='cpu')
-    
+    summary(model, (100,), device="cpu")
--- utils/load.py
+++ utils/load.py
@@ -1,7 +1,13 @@
 from typing import List
 
+import os
 import h5py
+from hbs.conformal_welding import ConformalWelding
 import numpy as np
+from hbs import get_hbs
+from hbs.boundary import get_boundary
+
+# from hbs.conformal_welding import ConformalWelding
 
 
 def load_cw(mat_path: str, class_names: List[str] = []) -> np.ndarray:
@@ -21,6 +27,33 @@ def load_cw(mat_path: str, class_names: List[str] = []) -> np.ndarray:
     conformal_weldings = np.array(conformal_weldings)
     return conformal_weldings
 
+
+def load_from_img(img_path: str, bound_point_num=500, cw_point_num=100):
+    bound = get_boundary(img_path, bound_point_num)
+    hbs, he, cw, disk = get_hbs(bound, 1000, 0.01)
+    cw.linear_interp(cw_point_num)
+    return cw, bound
+
+def load_from_dir(img_dir: str, bound_point_num=500, cw_point_num=100, exclude_list=[]):
+    data = dict()
+    for img_name in os.listdir(img_dir):
+        if not img_name.endswith('.png') and not img_name.endswith('.jpg'):
+            continue
+
+        if img_name in exclude_list:
+            continue
+
+        print(img_name)
+        img_path = os.path.join(img_dir, img_name)
+        data[img_name] = load_from_img(img_path, bound_point_num, cw_point_num)
+    
+    result = np.stack([preprocess(cw) for _, (cw, _) in data.items()])
+    return result, data
+
+def preprocess(cw: ConformalWelding) -> np.ndarray:
+    theta = cw.get_y_angle_diff()
+    theta = np.log(1 / theta)
+    return theta
 
 def loading_preprocess(theta: np.ndarray) -> np.ndarray:
     theta = np.insert(theta, 100, 2 * np.pi)
